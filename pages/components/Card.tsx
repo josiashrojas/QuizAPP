d43a014c07e614f77baa404224677ad76944c25b
josiashrojas <josias.h.rojas@gmail.com>
2024-01-31 17:49:20 -0300

    fix:color transition bug on hover

--- pages/components/Card.tsx
+++ pages/components/Card.tsx
@@ -1,6 +1,8 @@
-export default function Card(props: {color: string, answer: string,}){
+export default function Card(props: {className: string, answer: string,}){
     return (
-        <div className={`flex w-full h-full shadow-md justify-center items-center  bg-${props.color}-100 hover:bg-${props.color}-300 transition-colors ease-linear  min-h-60 rounded-md`}>
+        <div 
+            className={`flex w-full h-full shadow-md justify-center items-center transition-colors ease-linear  min-h-60 rounded-md ${props.className}`}
+        >
             <p>{props.answer}</p>
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,18 @@
 import Card from "./components/Card";
 
+const url = "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple"
+
+interface QuestionData {
+  correct_answer: string,
+  incorrect_answers: Array<string>
+}
+
+interface Questions {
+  results: Array<QuestionData>
+}
+
 export default function Home() {
+
   return (
     <div className="max-w-screen-lg mx-auto mt-10">
       <h1 className="text-center text-2xl font-bold text-gray-400">Quiz APP</h1>
@@ -8,10 +20,10 @@ export default function Home() {
         Question to answer
       </h2>
       <div className="grid grid-cols-2 grid-rows-2 gap-5">
-        <Card color="red" answer="Answer 1"/>
-        <Card color="blue" answer="Answer 2"/>
-        <Card color="yellow" answer="Answer 3"/>
-        <Card color="green" answer="Answer 4"/>
+        <Card className="bg-red-100 hover:bg-red-300" answer="Answer 1"/>
+        <Card className="bg-blue-100 hover:bg-blue-300"answer="Answer 2"/>
+        <Card className="bg-yellow-100 hover:bg-yellow-300" answer="Answer 3"/>
+        <Card className="bg-green-100 hover:bg-green-300" answer="Answer 4"/>
       </div>
     </div>
   );
