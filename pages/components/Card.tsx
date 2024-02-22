4ddb65114d0e4cb2c5b9ac40bdf1bd4da1055b4f
josiashrojas <josias.h.rojas@gmail.com>
2024-02-22 10:16:55 -0300

    feature:show if answer is correct or not

--- pages/components/Card.tsx
+++ pages/components/Card.tsx
@@ -1,7 +1,13 @@
-export default function Card(props: {className: string, answer: string,}){
+import { MouseEventHandler } from "react";
+
+export default function Card(props: {className: string, answer: string, onClick:(answer:string)=>void}){
     return (
         <div 
             className={`flex w-full h-full shadow-md justify-center items-center transition-colors ease-linear  min-h-60 rounded-md text-lg ${props.className}`}
+            onClick={(e)=>{
+                e.preventDefault()
+                props.onClick(props.answer)
+            }}
         >
             <p>{props.answer}</p>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,8 @@ import { useState, useEffect,useRef } from "react";
 export default function Home() {
 
   const [questions, setQuestions] = useState<Question[]>([])
+  const [showAnswer, setShowAnswer] = useState(false)
+  const [isCorrectAnswer, setIsCorrectAnswer] = useState(false)
   const initialized = useRef(false)
   
   useEffect(()=> {
@@ -16,18 +18,37 @@ export default function Home() {
     }
   }, [])
 
+  const handleClick = (answer:string) => {
+    if(answer === questions[0].correct_answer) {
+      setIsCorrectAnswer(true)
+    } else {
+      setIsCorrectAnswer(false)
+    }
+    setShowAnswer(true)
+  }
+
   return (
-    <div className="max-w-screen-lg mx-auto mt-10">
-      <h1 className="text-center text-2xl font-bold text-gray-400">Quiz APP</h1>
-      <h2 className="text-center text-5xl my-10">
-        {questions.length === 0 ? "loading..." : questions[0].question}
-      </h2>
-      <div className="grid grid-cols-2 grid-rows-2 gap-5">
-        <Card className="bg-red-100 hover:bg-red-300" answer={questions.length === 0 ? "loading..." : questions[0].allAnswers[0]}/>
-        <Card className="bg-blue-100 hover:bg-blue-300"answer={questions.length === 0 ? "loading..." : questions[0].allAnswers[1]}/>
-        <Card className="bg-yellow-100 hover:bg-yellow-300" answer={questions.length === 0 ? "loading..." : questions[0].allAnswers[2]}/>
-        <Card className="bg-green-100 hover:bg-green-300" answer={questions.length === 0 ? "loading..." : questions[0].allAnswers[3]}/>
-      </div>
-    </div>
+    <>
+      {
+        showAnswer ? 
+        <div className="w-full h-screen bg-white flex justify-center items-center">
+          <p className={`${isCorrectAnswer ? "text-green-400" : "text-red-400"} text-[120px] font-bold`}>{isCorrectAnswer ? "Correct" : "Incorrect"}</p>
+        </div>
+        :
+        <div className="max-w-screen-lg mx-auto mt-10">
+          <h1 className="text-center text-2xl font-bold text-gray-400">Quiz APP</h1>
+          <h2 className="text-center text-5xl my-10">
+            {questions.length === 0 ? "loading..." : questions[0].question}
+          </h2>
+          <div className="grid grid-cols-2 grid-rows-2 gap-5">
+            <Card onClick={handleClick} className="bg-red-100 hover:bg-red-300" answer={questions.length === 0 ? "loading..." : questions[0].allAnswers[0]}/>
+            <Card onClick={handleClick} className="bg-blue-100 hover:bg-blue-300"answer={questions.length === 0 ? "loading..." : questions[0].allAnswers[1]}/>
+            <Card onClick={handleClick} className="bg-yellow-100 hover:bg-yellow-300" answer={questions.length === 0 ? "loading..." : questions[0].allAnswers[2]}/>
+            <Card onClick={handleClick} className="bg-green-100 hover:bg-green-300" answer={questions.length === 0 ? "loading..." : questions[0].allAnswers[3]}/>
+          </div>
+        </div>
+      }
+    
+    </>
   );
 }
