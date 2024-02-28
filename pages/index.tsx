a0514767acfa427b44bd652a0e58494e0e2653c0
josiashrojas <josias.h.rojas@gmail.com>
2024-02-28 16:32:22 -0300

    fix:decode string with html encode

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import fetchQuestions, { Question } from "./api/fetchQuestions";
 import Card from "./components/Card";
 import { useState, useEffect,useRef } from "react";
+import { decode } from "html-entities";
 
 export default function Home() {
 
@@ -46,13 +47,13 @@ export default function Home() {
         <div className="max-w-screen-lg mx-auto mt-10">
           <h1 className="text-center text-2xl font-bold text-gray-400">Quiz APP</h1>
           <h2 className="text-center text-5xl my-10">
-            {questions.length === 0 ? "loading..." : questions[currentQuestion].question}
+            {questions.length === 0 ? "loading..." : decode(questions[currentQuestion].question)}
           </h2>
           <div className="grid grid-cols-2 grid-rows-2 gap-5">
-            <Card onClick={handleAnswerClick} className="bg-red-100 hover:bg-red-300" answer={questions.length === 0 ? "loading..." : questions[currentQuestion].allAnswers[0]}/>
-            <Card onClick={handleAnswerClick} className="bg-blue-100 hover:bg-blue-300"answer={questions.length === 0 ? "loading..." : questions[currentQuestion].allAnswers[1]}/>
-            <Card onClick={handleAnswerClick} className="bg-yellow-100 hover:bg-yellow-300" answer={questions.length === 0 ? "loading..." : questions[currentQuestion].allAnswers[2]}/>
-            <Card onClick={handleAnswerClick} className="bg-green-100 hover:bg-green-300" answer={questions.length === 0 ? "loading..." : questions[currentQuestion].allAnswers[3]}/>
+            <Card onClick={handleAnswerClick} className="bg-red-100 hover:bg-red-300" answer={questions.length === 0 ? "loading..." : decode(questions[currentQuestion].allAnswers[0])}/>
+            <Card onClick={handleAnswerClick} className="bg-blue-100 hover:bg-blue-300"answer={questions.length === 0 ? "loading..." : decode(questions[currentQuestion].allAnswers[1])}/>
+            <Card onClick={handleAnswerClick} className="bg-yellow-100 hover:bg-yellow-300" answer={questions.length === 0 ? "loading..." : decode(questions[currentQuestion].allAnswers[2])}/>
+            <Card onClick={handleAnswerClick} className="bg-green-100 hover:bg-green-300" answer={questions.length === 0 ? "loading..." : decode(questions[currentQuestion].allAnswers[3])}/>
           </div>
         </div>
         :
